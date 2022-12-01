79d915794df0e1ae7c754fe79b2ffa3a59ed2544
Pavan <101943293+Pavana004@users.noreply.github.com>
2022-12-01 19:30:32 +0530

    Update Books.js

--- src/container/books/Books.js
+++ src/container/books/Books.js
@@ -22,7 +22,7 @@ const Books = () => {
   
   
   useEffect(() => {
-    const url = "https://multiverse-ott-platform.herokuapp.com/api/allbooks";
+    const url = "https://multiverse-ott.onrender.com/api/allbooks";
     axios.get(url)
       .then((res) => {
         setBookUrl(res.data);
